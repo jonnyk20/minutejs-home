48759e3ec170d64751b0fb8c603887289d986f83
Jonny Kalambay <>
2018-02-02 22:21:48 -0800

    added content

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import 'src/assets/stylesheets/base.scss';
+import LessonsContainer from 'LessonsContainer';
 
+import 'src/assets/stylesheets/base.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App({ name }) {
   return (
-    <h1>Hello, {name}!</h1>
+    <div>
+      <h1>{name}</h1>
+      <h2>60 Second Javacript Lessons and More</h2>
+      <Intro>
+        A minute is way too short of time to learn some Javascript... <br />
+        But it should be enough to introduce and demo a topic. After that, use
+        the resouces listed with each video to learn more!
+      </Intro>
+      <LessonsContainer />
+    </div>
   );
-};
+}
+
+const Intro = styled.div`
+  border: solid 1ps black;
+`;
 
 App.propTypes = {
-  name: PropTypes.string,
+  name: PropTypes.string
 };
 
 export default App;
