1ccd8871140e3c7c63187476ea86370a6c81bd8f
Jonny Kalambay <>
2018-02-05 20:48:05 -0800

    Added script to manage instagram script

--- src/components/App/LessonsContainer.js
+++ src/components/App/LessonsContainer.js
@@ -28,6 +28,34 @@ const lessons = [
 ];
 
 class LessonsContainer extends Component {
+  state = {
+    isLoaded: false
+  };
+  componentWillMount() {
+    this.loadInstagram();
+  }
+
+  loadInstagram = () => {
+    if (!window.instgrm) {
+      const s = document.createElement('script');
+      s.async = s.defer = true;
+      s.src = 'https://platform.instagram.com/en_US/embeds.js';
+      s.id = 'react-instagram-embed-script';
+      s.onload = this.onLoad;
+      const body = document.body || null;
+      if (body) {
+        body.appendChild(s);
+      }
+    }
+  };
+
+  onLoad = () => {
+    console.log('Loaded instagram');
+    this.setState({
+      isLoaded: true
+    });
+  };
+
   renderLessons = () => {
     return lessons.map(lesson => {
       return <Lesson key={lesson.id}
